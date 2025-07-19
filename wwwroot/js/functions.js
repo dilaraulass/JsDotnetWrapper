db754d47e3bf49aa90c989078155295bb54c82c6
dilaraulass <dilaraulas16@gmail.com>
2025-07-19 21:02:21 +0300

    Form validation ve localStorage entegrasyonu tamamlandı

---
+++ wwwroot/js/functions.js
@@ -0,0 +1,17 @@
+﻿window.validateForm = (user, dotnetRef) => {
+    if (user.name && user.email && user.age > 0) {
+        const userList = JSON.parse(localStorage.getItem("users") || "[]");
+        userList.push(user);
+        localStorage.setItem("users", JSON.stringify(userList));
+        dotnetRef.invokeMethodAsync("ReceiveValidatedUser", user);
+    } else {
+        alert("Lütfen tüm alanları doldurun!");
+    }
+};
+
+window.getAllUsersFromStorage = (dotnetRef) => {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    users.forEach(u => {
+        dotnetRef.invokeMethodAsync("ReceiveValidatedUser", u);
+    });
+};
