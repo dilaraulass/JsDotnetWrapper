4038c139181a5f7ba42ccd8877bed1cbe9ee4016
dilaraulass <dilaraulas16@gmail.com>
2025-07-19 23:23:13 +0300

    uı güncelleştirmeleri

--- wwwroot/js/functions.js
+++ wwwroot/js/functions.js
@@ -1,7 +1,15 @@
 ﻿window.validateForm = (user, dotnetRef) => {
     if (user.name && user.email && user.age > 0) {
         const userList = JSON.parse(localStorage.getItem("users") || "[]");
-        userList.push(user);
+
+        // Aynı email varsa güncelleme
+        const existingIndex = userList.findIndex(u => u.email === user.email);
+        if (existingIndex !== -1) {
+            userList[existingIndex] = user;
+        } else {
+            userList.push(user);
+        }
+
         localStorage.setItem("users", JSON.stringify(userList));
         dotnetRef.invokeMethodAsync("ReceiveValidatedUser", user);
     } else {
@@ -14,4 +22,10 @@ window.getAllUsersFromStorage = (dotnetRef) => {
     users.forEach(u => {
         dotnetRef.invokeMethodAsync("ReceiveValidatedUser", u);
     });
+};
+
+window.deleteUserFromStorage = (email) => {
+    let users = JSON.parse(localStorage.getItem("users") || "[]");
+    users = users.filter(u => u.email !== email);
+    localStorage.setItem("users", JSON.stringify(users));
 };
